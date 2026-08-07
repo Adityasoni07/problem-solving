ec657ec33f1df218997b3e8d4a902ea476dc0783
Aditya Soni <220446967+Adityasoni07@users.noreply.github.com>
2026-08-08 00:14:04 +0530

    Solved LC

---
+++ LeetCode/Problems/0143_Reorder_List.java
@@ -0,0 +1,48 @@
+
+class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public void reorderList(ListNode head) {
+        Stack<ListNode> st = new Stack<>();
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode mid = slow;
+        while (slow != null) {
+            st.push(slow);
+            slow = slow.next;
+        }
+        ListNode point = head;
+        ListNode tempHead = new ListNode();
+        while (point != mid) {
+            ListNode temp = point.next;
+            tempHead.next = point;
+            point.next = st.pop();
+            tempHead = tempHead.next.next;
+            point = temp;
+        }
+        if (!st.isEmpty()) {
+            tempHead.next = st.pop();
+            tempHead = tempHead.next;
+        }
+        tempHead.next = null;
+    }
+}
