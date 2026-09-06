bfe6090ed90f338ee458d3dc1234a462768796d9
Aditya Soni <220446967+Adityasoni07@users.noreply.github.com>
2026-09-06 21:56:53 +0530

    Modified

--- LeetCode/Problems/0315_Count_of_Smaller_Numbers_After_Self.java
+++ LeetCode/Problems/0315_Count_of_Smaller_Numbers_After_Self.java
@@ -1,55 +1,45 @@
 import java.util.*;
 
 class Solution {
-    public class Pair {
-        int idx;
-        int val;
-
-        Pair(int idx, int val) {
-            this.idx = idx;
-            this.val = val;
-        }
-    }
-
     int counts[];
 
-    public void partition(Pair[] pairArr, int left, int right) {
+    public void partition(int nums[], int[] index, int left, int right) {
         if (left >= right) {
             return;
         }
         int mid = left + (right - left) / 2;
-        partition(pairArr, left, mid);
-        partition(pairArr, mid + 1, right);
-        merge(pairArr, left, mid, right);
+        partition(nums, index, left, mid);
+        partition(nums, index, mid + 1, right);
+        merge(nums, index, left, mid, right);
     }
 
-    public void merge(Pair[] pairArr, int left, int mid, int right) {
+    public void merge(int nums[], int[] index, int left, int mid, int right) {
         int i = left;
         int j = mid + 1;
         int k = 0;
-        Pair[] newPairArr = new Pair[right - left + 1];
+        int[] newArr = new int[right - left + 1];
         int rightSmallerCount = 0;
         while (i <= mid && j <= right) {
-            if (pairArr[i].val > pairArr[j].val) {
-                newPairArr[k++] = pairArr[j++];
+            if (nums[index[i]] > nums[index[j]]) {
+                newArr[k++] = index[j++];
                 rightSmallerCount++;
             } else {
-                newPairArr[k++] = pairArr[i];
-                counts[pairArr[i].idx] += rightSmallerCount;
+                newArr[k++] = index[i];
+                counts[index[i]] += rightSmallerCount;
                 i++;
             }
         }
         while (i <= mid) {
-            newPairArr[k++] = pairArr[i];
-            counts[pairArr[i].idx] += rightSmallerCount;
+            newArr[k++] = index[i];
+            counts[index[i]] += rightSmallerCount;
             i++;
         }
         while (j <= right) {
-            newPairArr[k++] = pairArr[j++];
+            newArr[k++] = index[j++];
         }
 
-        for (int x = 0; x < newPairArr.length; x++) {
-            pairArr[left + x] = newPairArr[x];
+        for (int x = 0; x < newArr.length; x++) {
+            index[left + x] = newArr[x];
         }
 
     }
@@ -57,11 +47,11 @@ class Solution {
     public List<Integer> countSmaller(int[] nums) {
         int n = nums.length;
         counts = new int[n];
-        Pair[] pairArr = new Pair[n];
+        int[] index = new int[n];
         for (int i = 0; i < n; i++) {
-            pairArr[i] = new Pair(i, nums[i]);
+            index[i] = i;
         }
-        partition(pairArr, 0, n - 1);
+        partition(nums, index, 0, n - 1);
         List<Integer> ans = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             ans.add(counts[i]);
